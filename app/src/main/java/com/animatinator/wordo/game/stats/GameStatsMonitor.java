452ea0e7e954a74571a8a079c8c1e59242803ac2
animatinator <animatinator@gmail.com>
2018-10-06 15:33:34 +0100

    Add javadoc to GameStatsMonitor
    Seems like good form.

--- app/src/main/java/com/animatinator/wordo/game/stats/GameStatsMonitor.java
+++ app/src/main/java/com/animatinator/wordo/game/stats/GameStatsMonitor.java
@@ -3,6 +3,10 @@ package com.animatinator.wordo.game.stats;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * Records statistics about a game. Because these are time-based, data can only be accessed in
+ * snapshot form by calling {@link GameStatsMonitor#getGameStatsNow()}.
+ */
 public class GameStatsMonitor {
     private Date startTime;
     private int numHints = 0;
@@ -11,15 +15,24 @@ public class GameStatsMonitor {
         startTime = Calendar.getInstance().getTime();
     }
 
+    /**
+     * Notify that the player used a hint.
+     */
     public void hintRequested() {
         numHints++;
     }
 
+    /**
+     * Take a snapshot of the stats.
+     */
     public GameStats getGameStatsNow() {
         Date endTime = Calendar.getInstance().getTime();
         return new GameStats(startTime, endTime, numHints);
     }
 
+    /**
+     * A snapshot of the game's statistics at a given time.
+     */
     public static final class GameStats {
         private final Date startTime;
         private final Date endTime;
